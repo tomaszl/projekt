34dcbe444ae14090ccd5cd4f4496bd526cbb7ad9
tomaszl <asqard@o2.pl>
2013-08-13 10:26:02 +0200

    akcje

--- reinfo/application/controllers/IndexController.php
+++ reinfo/application/controllers/IndexController.php
@@ -11,8 +11,9 @@ class IndexController extends Zend_Controller_Action
     public function indexAction()
     {
       $Glowna = new Application_Model_DbTable_Glowna();
-      $this->view->index = $Glowna->fetchAll();
+      $this->view->glowna = $Glowna->fetchAll();
     }
+
     public function onasAction()
     {
         $Onas = new Application_Model_DbTable_Onas();
@@ -24,29 +25,107 @@ class IndexController extends Zend_Controller_Action
     {
         // wdrozenia
     }
+
     public function blogAction()
     {
      // blog
         $Blog = new Application_Model_DbTable_Blog();
         $this->view->blog = $Blog->fetchAll();
     }
+
     public function postAction()
     {
-        $Blog = new Application_Model_DbTable_Blog();
+   //     $Blog = new Application_Model_DbTable_Blog();
       //  $this->view->blog = $Blog->fetchAll();
         
     }
+
     public function ofertaAction()
-    
     {
         // oferta
     }
+
     public function kontaktAction()
-    { 
+    {
         // kontakt
     }
-    
+// PANEL CRUD
+    public function createformAction()
+    {
+     $this->view->form = new Application_Form_Blog();
+    $url = $this->view->url(array('action' => 'create'));
+    $this->view->form->setAction($url);
     }
+
+    public function createAction()
+   {
+            if ($this->getRequest()->isPost()) {
+        $form = new Application_Form_Blog();
+        if ($form->isValid($this->getRequest()->getPost())) {
+            $data = $form->getValues();
+            $DbTable = new Application_Model_DbTable_Blog();
+            $id = $DbTable->insert($data);
+            return $this->_helper->redirector(
+                'edit', 'index', null, array('id' => $id)
+            );
+        }
+        $this->view->form = $form;
+    } else {
+        throw new Zend_Controller_Action_Exception('BĹÄdny adres!', 404);
+    }
+   
+}
+
+    public function deleteAction()
+    {
+        // action body
+    }
+
+    public function editAction()
+    {
+        $id = $this->getRequest()->getParam('id');  
+    $DbTable = new Application_Model_DbTable_Blog();  
+    $obj = $DbTable->find($id)->current();  
+    if (!$obj) {  
+        throw new Zend_Controller_Action_Exception('Błędny adres!', 404);  
+    }  
+    $this->view->form = new Application_Form_Blog();  
+    $this->view->form->populate($obj->toArray());  
+    $url = $this->view->url(array('action' => 'update', 'id' => $id));  
+    $this->view->form->setAction($url);  
+    $this->view->object = $obj;  
+    }
+
+    public function updateAction()
+    {
+        // action body
+    }
+
+    public function showAction()
+    {
+          $id = $this->getRequest()->getParam('id');
+    $DbTable = new Application_Model_DbTable_Blog();
+    $obj = $DbTable->find($id)->current();
+    if (!$obj) {
+        throw new Zend_Controller_Action_Exception('BĹÄdny adres!', 404);
+    }
+    $this->view->object = $obj;
+    }
+
+
+}
+
+
+
+
+
+
     
+
+
+
+
+
+
 
 
